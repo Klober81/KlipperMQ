ba579ff1f9a955d7903bc40469ad8cb23f3cc24e
Rob Niccum <klober@gmail.com>
2026-08-30 17:20:18 -0700

    mq_manager: Add runtime queue ownership registry (#2)
    Always-load with mq_config before toolhead. Exclusive owned_axes plus shareable QUEUE_CLAIM/QUEUE_RELEASE.

--- klippy/klippy.py
+++ klippy/klippy.py
@@ -122,6 +122,7 @@ class Printer:
         for section_config in config.get_prefix_sections(''):
             self.load_object(config, section_config.get_name(), None)
         self.load_object(config, 'mq_config')
+        self.load_object(config, 'mq_manager')
         for m in [toolhead]:
             m.add_printer_objects(config)
         # Validate that there are no undefined parameters in the config file
